2559024e902f34d8641f464ed2b07d4091b1804f
xxx663xxx <xxx663xxx@kimo.com>
2017-04-30 20:06:03 +0800

    Upadte Git ignore, add settings example file
    Upadte Git ignore, add settings example file

---
+++ config/settings.rb
@@ -0,0 +1,13 @@
+module Settings
+	LOG_PATH = 'Your log file path here'
+	API_URL = 'Your APP URL here'
+   
+	OAUTH_ACCESS_TOKEN_ENDPOINT = 'https://apitest.cc.ncu.edu.tw:5566/oauth/token'
+	
+	OAUTH_APP_ID = 'Your APP ID here'
+	OAUTH_SECRET = 'Your APP secret here'
+	
+	STATIC_FILE_DIR = '../public'
+	TMP_DIR = '../tmp'
+	   
+end
