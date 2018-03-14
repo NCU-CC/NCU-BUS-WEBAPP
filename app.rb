0d61c5b895ec1e0e862972755fdea8d9f4a3d5dd
djosix <djosicks@gmail.com>
2018-03-14 23:18:21 +0800

    Use space indent

--- app.rb
+++ app.rb
@@ -3,10 +3,10 @@ require './app/api/v2.rb'
 require 'grape_logging'
 
 module Bus
-	class API < Grape::API
-		logger.formatter = GrapeLogging::Formatters::Default.new
-		logger Logger.new GrapeLogging::MultiIO.new(STDERR, File.open(Settings::LOG_PATH, 'a'))
-		use GrapeLogging::Middleware::RequestLogger, { logger: logger }
-		mount Bus::V2 => '/'
-	end
+  class API < Grape::API
+    logger.formatter = GrapeLogging::Formatters::Default.new
+    logger Logger.new GrapeLogging::MultiIO.new(STDERR, File.open(Settings::LOG_PATH, 'a'))
+    use GrapeLogging::Middleware::RequestLogger, { logger: logger }
+    mount Bus::V2 => '/'
+  end
 end
--- app/api/v2.rb
+++ app/api/v2.rb
@@ -1,85 +1,85 @@
 module Bus
 
-	class V2 < Grape::API
-		format :txt	
+  class V2 < Grape::API
+    format :txt	
 
-		helpers do
+    helpers do
 
-			def getAccessToken
-				cache = JSON.parse(File.read(Settings::TMP_DIR + "/accessToken"))
-				cacheExpired = (cache['expires_in'] + cache['created_at'] - Time.new.to_i < 300)
+      def getAccessToken
+        cache = JSON.parse(File.read(Settings::TMP_DIR + "/accessToken"))
+        cacheExpired = (cache['expires_in'] + cache['created_at'] - Time.new.to_i < 300)
 
-				if !cacheExpired
-					cache['access_token']
-					
-				else
-					authString = Settings::OAUTH_APP_ID + ':' + Settings::OAUTH_SECRET
-					authCode = Base64.strict_encode64(authString)
+        if !cacheExpired
+          cache['access_token']
+          
+        else
+          authString = Settings::OAUTH_APP_ID + ':' + Settings::OAUTH_SECRET
+          authCode = Base64.strict_encode64(authString)
 
-					response = RestClient::Request.execute(
-						url: Settings::OAUTH_ACCESS_TOKEN_ENDPOINT,
-						method: :post,
-						payload: 'grant_type=client_credentials',
-						verify_ssl: false,
-						headers: {
-							Authorization: 'Basic ' + authCode
-						}
-					)
+          response = RestClient::Request.execute(
+            url: Settings::OAUTH_ACCESS_TOKEN_ENDPOINT,
+            method: :post,
+            payload: 'grant_type=client_credentials',
+            verify_ssl: false,
+            headers: {
+              Authorization: 'Basic ' + authCode
+            }
+          )
 
-					file = File.open(Settings::TMP_DIR + "/accessToken", "w")
-					file.write(response.body)
-					file.close()
+          file = File.open(Settings::TMP_DIR + "/accessToken", "w")
+          file.write(response.body)
+          file.close()
 
-					data = JSON.parse(response.body)
-					data['access_token']
-				end
-			end
-		end
+          data = JSON.parse(response.body)
+          data['access_token']
+        end
+      end
+    end
 
-		rescue_from :all do |e|
-			V2.logger.error e
-			error! 'server_error', 500
-		end
-
-
-
-		desc 'NCU_BUS WebApp, serve a webApp with access Token' do
-		end
-
-		params do
-		end
-
-		content_type :html, "text/html; charset=utf-8"
-
-		get '/' do
-			accessToken = getAccessToken()
-			header 'Content-Type', 'text/html; charset=UTF-8'
-			raw = File.read(Settings::STATIC_FILE_DIR + "/index.html")
-			raw = raw.gsub('Your access token here', accessToken)
-			raw = raw.gsub('Your api url here', Settings::API_URL)
-			ERB.new(raw).result(binding)
-		end
+    rescue_from :all do |e|
+      V2.logger.error e
+      error! 'server_error', 500
+    end
 
 
 
-		desc 'Refresh AccessToken' do
-		end
-		
-		params do
-		end
-		
-		content_type :txt, 'text/plain'
-		
-		get :accessToken do
-			accessToken = getAccessToken()
-		end
+    desc 'NCU_BUS WebApp, serve a webApp with access Token' do
+    end
+
+    params do
+    end
+
+    content_type :html, "text/html; charset=utf-8"
+
+    get '/' do
+      accessToken = getAccessToken()
+      header 'Content-Type', 'text/html; charset=UTF-8'
+      raw = File.read(Settings::STATIC_FILE_DIR + "/index.html")
+      raw = raw.gsub('Your access token here', accessToken)
+      raw = raw.gsub('Your api url here', Settings::API_URL)
+      ERB.new(raw).result(binding)
+    end
 
 
-		add_swagger_documentation api_version: 'v2',
-		hide_documentation_path: true,
-		hide_format: true,
-		mount_path: '/doc',
-		base_path: "#{Settings::API_URL}/bus/v2"
 
-	end
+    desc 'Refresh AccessToken' do
+    end
+    
+    params do
+    end
+    
+    content_type :txt, 'text/plain'
+    
+    get :accessToken do
+      accessToken = getAccessToken()
+    end
+
+
+    add_swagger_documentation api_version: 'v2',
+    hide_documentation_path: true,
+    hide_format: true,
+    mount_path: '/doc',
+    base_path: "#{Settings::API_URL}/bus/v2"
+
+  end
 end
